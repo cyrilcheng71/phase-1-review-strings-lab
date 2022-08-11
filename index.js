50a953bc5ff4bd52c02b1d88ffc356c0e417a020
Man Cheng <cyrilcheng@protonmail.com>
2022-08-11 14:24:39 -0700

    commit tree ff072dae6d5e9ecdcc0d5da6c9b5a78078e9e4cb

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+let currentUser ='Cyril';
+const welcomeMessage = 'Welcome to Flatbook, ' 
++ currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+
+const userInitial = currentUser.slice(0,1)
+
+const shortGreeting = `'Welcome, ${userInitial}!`;
+
